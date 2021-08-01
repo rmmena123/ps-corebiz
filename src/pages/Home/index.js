96e119316027d25405bf6083ef7e4edae57c6827
Rodrigo Machado Mena <rm.mena86@gmail.com>
2021-07-31 22:49:36 -0300

    Componente Banner #1
    - Versão Desktop Implementada

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,39 +1,61 @@
-// Importações necessárias
+// --- Importações necessárias
 import React from "react";
 import axios from "axios";
 import * as S from "./styles";
 import "./styles.css";
 
-// Importação das imagens
+// --- Importação das imagens
+// Header
 import LogoBlack from "../../img/logo-corebiz.png";
 import SearchImage from "../../img/search.png";
 import UserImage from "../../img/user.png";
 import CartImage from "../../img/cart.png";
 
-// Função que retorna a página
+// Main - Banner
+import Banner from "../../img/banner.png";
+import BannerMobile from "../../img/banner-mobile.png";
+
+// --- Função que retorna a página
 function App() {
 	return (
-		// ---------- Header ----------
-		<S.Header>
-			<img src={LogoBlack} alt="Logo Corebiz"></img>
+		<>
+			<S.Header>
+				<img src={LogoBlack} alt="Logo Corebiz"></img>
 
-			<S.HeaderComponent className="userInput">
-				<input placeholder="O que está procurando?"></input>
-				<img src={SearchImage} alt="Ícone Lupa Pesquisa"></img>
-			</S.HeaderComponent>
+				<S.HeaderComponent className="userInput">
+					<input placeholder="O que está procurando?"></input>
+					<img src={SearchImage} alt="Ícone Lupa Pesquisa"></img>
+				</S.HeaderComponent>
 
-			<S.HeaderComponent>
-				<img src={UserImage} alt="Ícone Usuário"></img>
-				<S.HeaderParagraph>Minha Conta</S.HeaderParagraph>
+				<S.HeaderComponent>
+					<img src={UserImage} alt="Ícone Usuário"></img>
+					<S.HeaderParagraph>Minha Conta</S.HeaderParagraph>
 
-				<img
-					src={CartImage}
-					alt="Ícone Carrinho de Compras"
-					className="imgHeader"
-				></img>
-				<S.HeaderParagraph className="cartItems">0</S.HeaderParagraph>
-			</S.HeaderComponent>
-		</S.Header>
+					<img
+						src={CartImage}
+						alt="Ícone Carrinho de Compras"
+						className="imgHeader"
+					></img>
+					<S.HeaderParagraph className="cartItems">0</S.HeaderParagraph>
+				</S.HeaderComponent>
+			</S.Header>
+
+			<S.Main>
+				<S.Banner>
+					<img src={Banner} alt="Banner" className="bannerDesktop"></img>
+					<img
+						src={BannerMobile}
+						alt="Banner Mobile"
+						className="bannerMobile"
+					></img>
+					<S.BannerParagraph>
+						<p>Olá, o que você está buscando?</p>
+
+						<p>Criar ou migrar seu e-commerce?</p>
+					</S.BannerParagraph>
+				</S.Banner>
+			</S.Main>
+		</>
 	);
 }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -9,6 +9,10 @@ export const Header = styled.header`
 	align-items: center;
 	margin-top: 1rem;
 
+	img {
+		cursor: pointer;
+	}
+
 	@media (max-width: 800px) {
 		flex-direction: column;
 	}
@@ -26,6 +30,10 @@ export const HeaderComponent = styled.div`
 		font-size: 1.3rem;
 	}
 
+	img {
+		cursor: pointer;
+	}
+
 	@media (max-width: 800px) {
 		margin-top: 2rem;
 	}
@@ -35,4 +43,42 @@ export const HeaderParagraph = styled.p`
 	font-size: 1.3rem;
 	margin-left: 0.6rem;
 	cursor: pointer;
+`;
+
+// ---------- Main ----------
+
+export const Main = styled.main`
+	width: 100%;
+`;
+
+// ----- Banner -----
+
+export const Banner = styled.section`
+	display: flex;
+	margin-top: 4rem;
+	height: 43rem;
+
+	img {
+		width: 100%;
+		height: 100%;
+		position: relative;
+	}
+`;
+
+export const BannerParagraph = styled.div`
+	position: absolute;
+	left: 5%;
+	top: 25%;
+	width: 50%;
+
+	p {
+		color: white;
+		font-size: 3rem;
+		font-weight: 700;
+	}
+
+	p:last-child {
+		font-size: 4.5rem;
+		font-weight: 900;
+	}
 `;
